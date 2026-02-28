34308261cfed9eca66029f2f699d331a9bf599fc
5p4u4net-del <5p4u4net@gmail.com>
2026-02-28 21:23:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // Google Apps Script 部署後的網址
   // 格式：https://script.google.com/macros/s/【YOUR_SCRIPT_ID】/exec
   // 部署步驟：Apps Script → 部署 → 新增部署作業 → 網頁應用程式
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzJms5OJT-WNLeStfH_6Vuba-vULTGzY0IQJVgZtIYGwtVz40Bjr364hjh6QXY5PnAl/exec',
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyr9AQLVtLslBXC_uWMAtpquR50YFCqiy8GoFDFMqnLcFpZ3l2E3QvkJ7BPm8C5q0Wq/exec',
 
   // Session 儲存 key（不需要修改）
   SESSION_KEY: 'dashboard_session',
